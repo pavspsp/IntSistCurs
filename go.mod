fb8af50e1cc56d2e50546a671e45786b8c62fbd6
pavspsp <sppavelsp000@gmail.com>
2024-11-04 01:52:01 +0300

    First

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/IntSistCurs
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hell oh...")
+	time.Sleep(3 * time.Second)
+	fmt.Print("ff\n")
+}
